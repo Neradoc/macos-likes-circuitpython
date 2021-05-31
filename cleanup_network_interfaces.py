af19f97743e8d7ea52956f2e7c2752ea4142c9c6
Neradoc <neraOnGit@ri1.fr>
2021-05-31 06:25:01 +0200

    use click for color out, confirm and options

--- cleanup_network_interfaces.py
+++ cleanup_network_interfaces.py
@@ -9,53 +9,63 @@ Might not work on Big Sur's heightened security.
 Not tested with a reboot.
 There might be a better way to do that using the defaults command, once the python script has found all the service_id to remove.
 """
+import click
+import os
 import plistlib
 import subprocess
 
 preferences_file = "/Library/Preferences/SystemConfiguration/preferences.plist"
 output_file = "output_preferences.plist"
 
-with open(preferences_file,"rb") as fp:
-	data = plistlib.load(fp)
+@click.group(invoke_without_command=True)
+@click.option(
+    "--yes", is_flag=True, help="Validate without warning."
+)
+def main(yes):
+	with open(preferences_file,"rb") as fp:
+		data = plistlib.load(fp)
 
-removed_services = []
-services = list(data['NetworkServices'].keys())
+	removed_services = []
+	services = list(data['NetworkServices'].keys())
 
-for service_id in services:
-	service = data['NetworkServices'][service_id]
+	for service_id in services:
+		service = data['NetworkServices'][service_id]
 
-	try:
-		name = service['UserDefinedName']
-	except:
-		continue
+		try:
+			name = service['UserDefinedName']
+		except:
+			continue
 
-	try:
-		interface = service['Interface']['DeviceName']
-	except:
-		print(f"{name} - Keep")
-		continue
+		try:
+			interface = service['Interface']['DeviceName']
+		except:
+			print(f"{name} - Keep")
+			continue
 
-	keep = not interface.startswith("usbmodem")
-	
-	print(f"{name} ({interface}) - ",end="")
-	if keep:
-		print("Keep")
+		keep = not interface.startswith("usbmodem")
+
+		if keep:
+			click.secho(f"{name} ({interface}) - Keep", fg="green")
+		else:
+			click.secho(f"{name} ({interface}) - Remove", fg="red")
+			removed_services.append(name)
+			del data['NetworkServices'][service_id]
+
+	with open(output_file,"wb") as fp:
+		plistlib.dump(data,fp)
+
+	print("")
+	if removed_services:
+		if os.getuid() == 0:
+			if yes or click.confirm("Do you want to validate the changes ?"):
+				subprocess.run(["sudo", "cp", output_file, preferences_file])
+				print("Done.")
+		else:
+			print(f"Run this script with sudo or do:")
+			print(f"sudo cp {output_file} {preferences_file}")
 	else:
-		print("Remove")
-		removed_services.append(name)
-		del data['NetworkServices'][service_id]
+		print("No service removed, nothing to do")
 
-with open(output_file,"wb") as fp:
-	plistlib.dump(data,fp)
 
-print("")
-if removed_services:
-	print(f"And now:\nsudo cp {output_file} {preferences_file}")
-	print("Press ctrl-C to cancel, or press enter and type admin password when asked")
-	input()
-	try:
-		subprocess.run(["sudo", "cp", output_file, preferences_file])
-	except KeyboardInterrupt:
-		pass
-else:
-	print("No service removed, nothing to do")
+if __name__ == "__main__":
+	main()

197a51c7711e9a2d1e19ec3509e7cf0263278c6d
Neradoc <neraOnGit@ri1.fr>
2021-10-24 05:59:15 +0200

    add script to delete hidden files (discotool is better though)

---
+++ cleanup_hidden_files.py
@@ -0,0 +1,40 @@
+"""
+TODO: an applescript app/droplet version
+"""
+import os
+import click
+default_drive = "/Volumes/CIRCUITPY"
+
+def tree_clean(root, force=False):
+	for target in os.listdir(root):
+		file = os.path.join(root,target)
+		if os.path.isdir(file):
+			tree_clean(file)
+		else:
+			if os.path.basename(file).startswith("._"):
+				if force:
+					click.echo(f"Deleting {file}")
+					os.remove(file)
+				elif click.confirm(f"Delete {file} ?"):
+					os.remove(file)
+
+@click.group(invoke_without_command=True)
+@click.option(
+	"--path",
+	default=default_drive,
+	help="#######",
+)
+@click.option(
+	"--force",
+	is_flag=True,
+	help="#######",
+)
+@click.pass_context
+def main(ctx, path, force):
+	if os.path.isdir(path):
+		tree_clean(path, force)
+	else:
+		print(f"Drive {path} not found")
+
+if __name__ == "__main__":
+	main()
